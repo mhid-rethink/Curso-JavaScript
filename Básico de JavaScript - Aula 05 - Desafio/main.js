380b04c5cf4906a5bd5c855483f6fd5abe9cc1de
mhid-rethink <matheus.dias@rethink.dev>
2023-02-22 16:16:32 -0300

    Adequando código para clean code
    Alterando nomes de variáveis de português para inglês

--- Básico de JavaScript - Aula 05 - Desafio/main.js
+++ Básico de JavaScript - Aula 05 - Desafio/main.js
@@ -1,61 +1,43 @@
 import {pokemons} from "./data.js";
-
-const busca = (arr, prop, valor) => {
-    let pokemon = [];
-    for(let i = 0; i < arr.length; i++) {
-        if (!(arr[i][prop] instanceof Array) && arr[i][prop] == valor) {
-            pokemon.push(arr[i])
-        } else if ((arr[i][prop] instanceof Array) && arr[i][prop].includes(valor)) {
-            pokemon.push(arr[i]);
+import {typesTranslation} from "./typesTranslation.js";
+const search = (originalPokemonArray, property, propertyValue) => {
+    let pokemonArray = [];
+    for(let i = 0; i < originalPokemonArray.length; i++) {
+        if (!(originalPokemonArray[i][property] instanceof Array) && originalPokemonArray[i][property] == propertyValue) {
+            pokemonArray.push(originalPokemonArray[i])
+        } else if ((originalPokemonArray[i][property] instanceof Array) && originalPokemonArray[i][property].includes(propertyValue)) {
+            pokemonArray.push(originalPokemonArray[i]);
         }
     }
-    if (pokemon) {
-        return pokemon
+    if (pokemonArray) {
+        return pokemonArray
     }  else {
         return "Who's that pokemon?"
     }
 }
 
-const encontraItensUnicosArray = (arr,prop) => {
+const findUniqueItensFromArray = (originalPokemonArray,property) => {
     const types = []
-    for(let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i][prop].length; j++) {
-            if (!types.includes(arr[i][prop][j])) {
-                types.push(arr[i][prop][j]);
+    for(let i = 0; i < originalPokemonArray.length; i++) {
+        for (let j = 0; j < originalPokemonArray[i][property].length; j++) {
+            if (!types.includes(originalPokemonArray[i][property][j])) {
+                types.push(originalPokemonArray[i][property][j]);
             }
         }
     }
     return types
 }
 
-const traduzType = (arr) => {
-    const types = {
-        "grass": "Planta",
-        "poison":"Venenoso",
-        "fire":"Fogo",
-        "flying":"Voador",
-        "water":"Água",
-        "bug":"Inseto",
-        "normal":"Normal",
-        "electric":"Elétrico",
-        "ground":"Terra",
-        "fairy":"Fada",
-        "fighting":"Lutador", 
-        "psychic":"Psíquico", 
-        "rock":"Pedra", 
-        "steel":"Metálico", 
-        "ice":"Gelo", 
-        "ghost":"Fantasma"
-    }
-    for(let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i]["type"].length; j++) {
-            if ( types[ arr[i]["type"][j] ] ) {
-                arr[i]["type"][j] = ( types[ arr[i]["type"][j] ] );
+const translateTypeToPortuguese = (originalPokemonArray) => {
+    for(let i = 0; i < originalPokemonArray.length; i++) {
+        for (let j = 0; j < originalPokemonArray[i]["type"].length; j++) {
+            if ( typesTranslation[ originalPokemonArray[i]["type"][j] ] ) {
+                originalPokemonArray[i]["type"][j] = ( typesTranslation[ originalPokemonArray[i]["type"][j] ] );
             }
         }
     }
-    return pokemons
+    return originalPokemonArray;
 }
-console.log(busca(pokemons, "name","ekans"));
-console.log(encontraItensUnicosArray(pokemons,"type"));
-console.log(traduzType(pokemons));
+console.log(search(pokemons, "name","ekans"));
+console.log(findUniqueItensFromArray(pokemons,"type"));
+console.log(translateTypeToPortuguese(pokemons));
