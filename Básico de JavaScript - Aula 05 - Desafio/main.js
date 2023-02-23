bdb029ac1deea88a9abde9a70aaed7e9df96e345
mhid-rethink <matheus.dias@rethink.dev>
2023-02-23 12:58:04 -0300

    Refatoração de codigo
    Refatoração de código, alterando código feito manualmente para funções nativas do JavaScript

--- Básico de JavaScript - Aula 05 - Desafio/main.js
+++ Básico de JavaScript - Aula 05 - Desafio/main.js
@@ -1,43 +1,18 @@
 import {pokemons} from "./data.js";
 import {typesTranslation} from "./typesTranslation.js";
-const resultPokemonSearch = (originalPokemonArray, property, propertyValue) => {
-    let pokemonArray = [];
-    for(let i = 0; i < originalPokemonArray.length; i++) {
-        if (!(originalPokemonArray[i][property] instanceof Array) && originalPokemonArray[i][property] == propertyValue) {
-            pokemonArray.push(originalPokemonArray[i])
-        } else if ((originalPokemonArray[i][property] instanceof Array) && originalPokemonArray[i][property].includes(propertyValue)) {
-            pokemonArray.push(originalPokemonArray[i]);
-        }
-    }
-    if (pokemonArray) {
-        return pokemonArray
-    }  else {
-        return "Who's that pokemon?"
-    }
-}
+const pokemonNameForSearch = "ekans";
+const pokemonTypeForSearch = "grass";
+const resultPokemonSearchByName = pokemons.filter(pokemon => pokemon.name == pokemonNameForSearch);
 
-const uniqueItensFromArray = (originalPokemonArray,property) => {
-    const types = []
-    for(let i = 0; i < originalPokemonArray.length; i++) {
-        for (let j = 0; j < originalPokemonArray[i][property].length; j++) {
-            if (!types.includes(originalPokemonArray[i][property][j])) {
-                types.push(originalPokemonArray[i][property][j]);
-            }
-        }
-    }
-    return types
-}
+const resultPokemonSearchByType = pokemons.filter(pokemon => pokemon.type.includes(pokemonTypeForSearch));
 
-const pokemonArrayWithTranslatedType = (originalPokemonArray) => {
-    for(let i = 0; i < originalPokemonArray.length; i++) {
-        for (let j = 0; j < originalPokemonArray[i]["type"].length; j++) {
-            if ( typesTranslation[ originalPokemonArray[i]["type"][j] ] ) {
-                originalPokemonArray[i]["type"][j] = ( typesTranslation[ originalPokemonArray[i]["type"][j] ] );
-            }
-        }
-    }
-    return originalPokemonArray;
-}
-console.log(resultPokemonSearch(pokemons, "name","ekans"));
-console.log(uniqueItensFromArray(pokemons,"type"));
-console.log(pokemonArrayWithTranslatedType(pokemons));
+const resultPokemonWithTranslatedType = pokemons.map((pokemon) => {
+    return {
+    ...pokemon,
+    type: pokemon.type.map(type => typesTranslation[type])
+}});
+
+// console.log(resultPokemonSearchByName);
+// console.log(resultPokemonSearchByType)
+console.log(resultPokemonWithTranslatedType);
+// console.log(pokemons)
