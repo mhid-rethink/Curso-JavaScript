adf19f932a7425229c4eda97177ccb103a72cd6c
mhid-rethink <matheus.dias@rethink.dev>
2023-05-05 12:51:50 -0300

    correcoes pós merge

--- fake_store_api_knex_csr/knexfile.ts
+++ fake_store_api_knex_csr/knexfile.ts
@@ -5,7 +5,7 @@ import type { Knex } from "knex";
 const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./dev.sqlite3",
+    filename: "./src/dev.sqlite3",
   },
   migrations: {
     directory: "src/database",
--- fake_store_api_knex_csr/src/repositories/categoryRepositories.ts
+++ fake_store_api_knex_csr/src/repositories/categoryRepositories.ts
@@ -58,5 +58,4 @@ export default {
   insertCategory,
   updateCategory,
   deleteCategory,
-  selectCategoryById,
 };
